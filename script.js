9dd2ef1594cdd3af5701c91af87bb6a12a7c2f2a
miwanoff <marina.iva.noffa@gmail.com>
2022-09-18 13:39:48 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let n = 13;
+while (n > 0) {
+  alert("Your turn!");
+  let m = prompt("Get the matches!");
+  /** todo isNaN() m <= 0 or m>3 */
+  n -= m;
+  alert(n);
+  if (n <= 0) {
+    alert("Your win!");
+    break;
+  }
+  alert("My turn!");
+  let random = Math.floor(Math.random() * 3) + 1;
+  /* todo */
+}
