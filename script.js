514c5ceb8aecfa0ea9e280d0368d2132e38f3f95
miwanoff <marina.iva.noffa@gmail.com>
2022-11-13 18:27:19 +0200

    Add script

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+
 let n = 13;
 while (n > 0) {
   alert("Your turn!");
   let m = prompt("Get the matches!");
   /** todo isNaN() m <= 0 or m>3 */
   n -= m;
-  alert(n);
+  alert(n); // ||||||
   if (n <= 0) {
     alert("Your win!");
     break;
@@ -12,4 +13,5 @@ while (n > 0) {
   alert("My turn!");
   let random = Math.floor(Math.random() * 3) + 1;
   /* todo */
+  alert(n); // ||||||
 }
